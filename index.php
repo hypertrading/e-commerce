b86fb538d8df1d4edd5740de98b0ab90f4b5ccb4
Alexis LE NAOURESE <ale-naou@e1r6p3.42.fr>
2016-04-09 17:24:40 +0200

    Dirty version .html. Plzzzzz do not judge

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include ("header.php"); ?>
+<section class="body">
+	<div class="framestuhome">
+		<div id="framestuhomeimg">IMG</div>
+		<div class="framestuhomeinfoblock">
+			<div class="framestuhomeinfo">Price</div>
+			<div class="framestuhomeinfo">LVL</div>
+		</div>
+		<div id="framestuhomecart">Cart</div>
+	</div>
+</section>
+</body>
