0d47f935c684d71998c11149ce4f2a3693753a4a
Alexis LE NAOURESE <ale-naou@e1r6p3.42.fr>
2016-04-10 01:10:13 +0200

    Small modifs

--- create.php
+++ create.php
@@ -1,28 +1,30 @@
 <?php
 include "functions.php";
+$return = "lol";
 if ($_POST[submit] != "OK" || $_POST[login] == "" || $_POST[passwd] == "")
-	echo "ERROR CREATE_USER (form empty or partially)\n";
+	$return = "ERROR CREATE_USER (form empty or partially)\n";
 else
 {
-	if (check_form($_POST[login] == FALSE) || check_form($_POST[passwd] == FALSE))
-		echo "Wrong Format\n";
+	if (check_form($_POST[login]) == FALSE || check_form($_POST[passwd]) == FALSE)
+		$return = "Wrong Format\n";
 	else
 	{
 		if (!$db = db_init())
-			echo "DB unloaded\n";
+			$return = "DB unloaded\n";
 		else
 		{
-			if (query_get_one_user($db, $_POST[login]) == NULL)
+			if (query_get_one_user($db, $_POST[login]) != $_POST[login])
 			{
 				$passwd = hash("whirlpool", $_POST[passwd]);
 				if (query_add_new_user($db, $_POST[login], $passwd) == FALSE)
-					echo "FAILED TO ADD USER IN DB";
+					$return = "FAILED TO ADD USER IN DB";
 				else
-					echo "SUCCESS";
+					$return = "SUCCESS";
 			}
 			else
-				echo "USER ALREADY EXIST";
+				$return = "USER ALREADY EXIST";
 		}
 	}
 }
+echo $return;
 ?>
