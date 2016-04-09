4b7cd2874100f8967745b985bca1a897170a1a1f
Valentin KLEPPER <vklepper@e1r6p2.42.fr>
2016-04-09 13:13:47 +0200

    Initial commit

---
+++ home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
+    <title>Rent a Student</title>
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <div>
+        <img class="logo" src="assets/img/logo_rent.jpg">
+        <button><a href="#">Log-in</a></button>
+        <button><a href="#">Register</a></button>
+        <div class="panier">
+            <a href="#"><img class="logo-panier" src="assets/img/panier.png"></a>
+            <p>3 students</p>
+            <p>200 Wallets</p>
+        </div>
+        <div class="clear"></div>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">All Students</a></li>
+            <li><a href="#">Favorite</a></li>
+        </ul>
+    </nav>
+</header>
