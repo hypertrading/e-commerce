a64fea183bb7a5c8de9f98c5f7895b0b996130f8
Alexis LE NAOURESE <ale-naou@e1r6p3.42.fr>
2016-04-09 18:03:04 +0200

    TIENS PREND CA !

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php include ("header.php"); ?>
 <section class="body">
-	<div class="framestuhome">
-		<div id="framestuhomeimg">IMG</div>
-		<div class="framestuhomeinfoblock">
-			<div class="framestuhomeinfo">Price</div>
-			<div class="framestuhomeinfo">LVL</div>
+		<div class="framestuhome">
+			<div id="framestuhomeimg"><img src="https://cdn.intra.42.fr/userprofil/ale-naou.jpg"></img></div>
+			<div class="framestuhomeinfoblock">
+				<div class="framestuhomeinfo"><p>price</p></div>
+				<div class="framestuhomeinfo"><p>lvl</p></div>
+			</div>
+			<div id="framestuhomecart">Cart</div>
 		</div>
-		<div id="framestuhomecart">Cart</div>
-	</div>
+		<div class="framestuhome">
+			<div id="framestuhomeimg"><img src="https://cdn.intra.42.fr/userprofil/ale-naou.jpg"></img></div>
+			<div class="framestuhomeinfoblock">
+				<div class="framestuhomeinfo"><p>price</p></div>
+				<div class="framestuhomeinfo"><p>lvl</p></div>
+			</div>
+			<div id="framestuhomecart">Cart</div>
+		</div>
+		<div class="framestuhome">
+			<div id="framestuhomeimg"><img src="https://cdn.intra.42.fr/userprofil/ale-naou.jpg"></img></div>
+			<div class="framestuhomeinfoblock">
+				<div class="framestuhomeinfo"><p>price</p></div>
+				<div class="framestuhomeinfo"><p>lvl</p></div>
+			</div>
+			<div id="framestuhomecart">Cart</div>
+		</div>
 </section>
 </body>
