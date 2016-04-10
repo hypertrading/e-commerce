3de204fc6e3536eb8bd2298f3d66da910be6a771
vklepper <vklepper@student.42.fr>
2016-04-10 17:34:07 +0200

    valid command ok, add css in all page

--- login.php
+++ login.php
@@ -1,9 +1,11 @@
 <?php
 include "header.php";
 ?>
-<form method="post" action="logging.php">
-	Identifiant: <input type="text" name="login" placeholder="login"/>
-	<br />
-	Mot de passe: <input type="password" name="passwd" placeholder="password" />
-	<input type="submit" name="submit" value="OK" />
-</form>
+<section class="body">
+	<form method="post" action="logging.php">
+		Identifiant: <input type="text" name="login" placeholder="login"/>
+		<br />
+		Mot de passe: <input type="password" name="passwd" placeholder="password" />
+		<input type="submit" name="submit" value="OK" />
+	</form>
+</section>
--- valid_command.php
+++ valid_command.php
@@ -1,7 +1,15 @@
 <?php
+session_start();
+include "functions.php";
 if($_GET['id'])
 {
-    
+    if ($db = db_init())
+    {
+        $query = "UPDATE `commands` SET `status`=1 WHERE `id`='" . $_GET['id'] . "'";
+        mysqli_query($db, $query);
+        $_SESSION['msg'] = "Command validate";
+        header('Location: admin.php');
+    }
 }
 else
 {
