05d7c6aaac5e229455f89dfa9667abf85501a1df
Alexis LE NAOURESE <ale-naou@e1r6p3.42.fr>
2016-04-10 15:10:59 +0200

    Take it

--- admin.php
+++ admin.php
@@ -9,14 +9,22 @@ include "header.php"
 		<td>Photo: <input type="text" name="img" placeholder="link" /></td>
 		<td>Prix: <input type="text" name="price" placeholder="price" /></td>
 		<td>Niveau: <input type="text" name="lvl" placeholder="lvl" /></td>
-		<tr>Skill: <input type="text" name="skill" placeholder="skill" /></tr>
-		<tr><input type="submit" name="submit" value="OK" /></tr>
-		</form>
 	</tr>
+	<tr><td><ul>
+		<?php $tab = get_all_cat();
+		foreach ($tab as $tmp => $cat)
+		{?>
+		<li><label><?php echo $cat['nom']; ?><input type="checkbox" name="<?php echo $cat['id']?>" /></label></li>
+	<?php }?>
+</ul></td></tr>
+	<tr>
+		<td><input type="submit" name="submit" value="OK" /></td>
+	</tr>
+	</form>
 </table>
 <table>
 		Create categorie
-		<form method="post" action="logging.php">
+		<form method="post" action=".php">
 		<td>Nom: <input type="text" name="cat" placeholder="skill"/></td>
 		<td><input type="submit" name="submit" value="OK" /></td>
 		</form>
